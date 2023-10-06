f8f0496cc4a83e88557bf32d075e1976f02d71bb
roialrogue <144475675+roialrogue@users.noreply.github.com>
2023-10-06 15:03:51 -0400

    Add files via upload

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EncoderTest.java
@@ -0,0 +1,60 @@
+package org.firstinspires.ftc.teamcode.SummerCodingClass;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp(name = "EncoderTest")
+public class EncoderTest extends LinearOpMode {
+
+    Hardware robot = Hardware.getInstance();
+
+
+    public void runOpMode() {
+
+        robot.init(hardwareMap);
+        telemetry.addData("Status", "(Metal Pipe Noise)");
+        telemetry.update();
+
+        int position = 0;
+        boolean pressingA = false;
+        boolean pressingB = false;
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+
+        position = robot.demoMotor.getCurrentPosition();
+        robot.demoMotor.setPower(1);
+        robot.demoMotor.setTargetPosition(position);
+        robot.demoMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+            if (gamepad1.a && !pressingA) {
+            position += 3;
+            pressingA = true;
+            } else if (!gamepad1.a) {
+            pressingA = false;
+            }
+            if (gamepad1.b && !pressingB) {
+            position -= 3;
+            pressingB = true;
+            } else if (!gamepad1.b) {
+            pressingB = false;
+            }
+
+            telemetry.addData("Position", position);
+            telemetry.addData("ActualMotorPosition", robot.demoMotor.getCurrentPosition());
+            telemetry.update();
+
+
+        }
+
+
+
+
+    }
+
+
+
+
+}
