e116a4090718960c28c99ffccb865b5019b0a857
asher <asherplihal1@gamil.com>
2023-12-28 15:24:34 -0500

    Pushing new labels for hardware

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
@@ -54,7 +54,7 @@ public class DriveConstants {
      * motor encoders or have elected not to use them for velocity control, these values should be
      * empirically tuned.
      */
-    public static double kV = 1.0 / rpmToVelocity(MAX_RPM);
+    public static double kV = 1 / rpmToVelocity(MAX_RPM);
     public static double kA = 0;
     public static double kStatic = 0;
 
@@ -65,10 +65,10 @@ public class DriveConstants {
      * small and gradually increase them later after everything is working. All distance units are
      * inches.
      */
-    public static double MAX_VEL = 52.48291908330528;
+    public static double MAX_VEL = 52.48291908330528; //30
     public static double MAX_ACCEL = 52.48291908330528;
-    public static double MAX_ANG_VEL = Math.toRadians(214.78926857142858);
-    public static double MAX_ANG_ACCEL = Math.toRadians(214.78926857142858);
+    public static double MAX_ANG_VEL = Math.toRadians(214.78926857142858); //60
+    public static double MAX_ANG_ACCEL = Math.toRadians(214.78926857142858); //214.78926857142858
 
     /*
      * Adjust the orientations here to match your robot. See the FTC SDK documentation for details.
