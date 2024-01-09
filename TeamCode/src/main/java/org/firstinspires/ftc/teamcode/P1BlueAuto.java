b63bff890de6b73f09620d6969488ece89b61383
roialrogue <asherplihal1@gmail.com>
2024-01-09 16:00:33 -0500

    Done

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/P1BlueAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/P1BlueAuto.java
@@ -25,7 +25,6 @@ public class P1BlueAuto extends LinearOpMode{
         private GPTCamera detector;
         public void runOpMode() {
             telemetry.addData("Status", "Initailized");
-            telemetry.update();
             robot.init(hardwareMap);
             SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
             final boolean[] cameraWorked = {false};
