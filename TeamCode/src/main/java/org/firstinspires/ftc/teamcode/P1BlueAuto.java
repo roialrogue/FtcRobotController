331367853b661a90df360bc46d520403a03f45be
asher <asherplihal1@gamil.com>
2024-01-09 11:34:38 -0500

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/P1BlueAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/P1BlueAuto.java
@@ -1,11 +1,14 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.trajectory.Trajectory;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 import org.openftc.easyopencv.OpenCvCamera;
 import org.openftc.easyopencv.OpenCvCameraRotation;
 
@@ -16,6 +19,7 @@ public class P1BlueAuto extends LinearOpMode{
     ElapsedTime runtime = new ElapsedTime();
         public void runOpMode() {
             robot.init(hardwareMap);
+            SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
             final boolean[] cameraWorked = {false};
 
             Hardware.getInstance().camera.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
@@ -36,34 +40,35 @@ public class P1BlueAuto extends LinearOpMode{
                 }
             });
 
-            waitForStart();
-
-
 /*runtime.reset();
             while(runtime.seconds() < 5) {
             hw.leftForwardWheel.setPower(1);
             }
             hw.leftForwardWheel.setPower(0);*/
 
-
-
             if (cameraWorked[0]) {
                 sleep(100); // Adjust this sleep time as needed
                 Hardware.getInstance().camera.stopStreaming(); //Watch this line
             }
+            waitForStart();
+            Trajectory BlueP2M1T1 = drive.trajectoryBuilder(new Pose2d(0,0))
+                    .lineToLinearHeading(new Pose2d(10,10, Math.toRadians(180)))
+                    .build();
 
             if (GPTCamera.leftSide == true) {
-                telemetry.addData("Found on the right", "");
-
+                telemetry.addData("Found in Auto on the right", "");
+                drive.followTrajectory(BlueP2M1T1);
             } else if (GPTCamera.middleSide == true) {
-                telemetry.addData("Found on the middle", "");
-
+                telemetry.addData("Found in Auto on the middle", "");
+                drive.followTrajectory(BlueP2M1T1);
             } else if (GPTCamera.rightSide == true) {
-                telemetry.addData("Found on the left", "");
-
+                telemetry.addData("Found in Auto on the left", "");
+                drive.followTrajectory(BlueP2M1T1);
             } else if (GPTCamera.nonSide == true) {
-                telemetry.addData("Did not find","");
+                telemetry.addData("Did not find in Auto","");
                 telemetry.update();
+                drive.followTrajectory(BlueP2M1T1);
             }
         }
+
     }
