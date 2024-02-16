6533059d686c9435681cf63149845cdd019cab35
roialrogue <asherplihal1@gmail.com>
2024-02-16 18:43:55 -0500

    Pushing new labels for hardware

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestApril.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestApril.java
@@ -1,13 +1,11 @@
 package org.firstinspires.ftc.teamcode;
 
 import android.util.Size;
-
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.acmerobotics.roadrunner.trajectory.Trajectory;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.controls.ExposureControl;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.controls.GainControl;
@@ -15,20 +13,14 @@ import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 import org.firstinspires.ftc.vision.VisionPortal;
 import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
 import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
-
-
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 @Autonomous(name = "Test Auto Camera")
 public class TestApril extends LinearOpMode {
-
-    int DESIRED_TAG_ID = 2;
     private AprilTagDetection desiredTag = null;
-
-    public static final double APRILTAG_BACKDROP_X              = 60.25;
-    public static final double APRILTAG_AUDIENCE_WALL_X         = -70.25;
-    public static final double BACKDROP_APRILTAG_DELTA_Y        = 6.0;
+    public static final double APRILTAG_BACKDROP_X = 60.25;
+    public static final double APRILTAG_AUDIENCE_WALL_X = -70.25;
     public static final Pose2d[] APRILTAG_POSES = new Pose2d[] {
             new Pose2d(APRILTAG_BACKDROP_X, 41.41, 90.0),        // TagId 1
             new Pose2d(APRILTAG_BACKDROP_X, 35.41, 90.0),        // TagId 2
@@ -91,36 +83,27 @@ public class TestApril extends LinearOpMode {
             telemetry.addData("#",tagDetections.size());
             for (AprilTagDetection desiredTag : tagDetections) {
                 if (desiredTag.metadata == null)continue;
-
                 Pose2d aprilTagPose = APRILTAG_POSES[desiredTag.id - 1];
+
                 double angleA = desiredTag.ftcPose.yaw + 90;
                 double angleB = 90 - desiredTag.ftcPose.bearing;
                 double sideA = desiredTag.ftcPose.range;
 
                 double angleC = 180 - angleA - angleB;
-
                 double sideB = (sideA * Math.sin(Math.toRadians(angleB))) / Math.sin(Math.toRadians(angleA));
-
                 double sideC = (sideA * Math.sin(Math.toRadians(angleC)))/ Math.sin(Math.toRadians(angleA));
 
-                double t2AngleA = angleA - 180;
-
+                double t2AngleA = 180 - angleA;
                 double t2AngleB = 90;
-
                 double t2AngleC = 180 - t2AngleA - t2AngleB;
-
                 double t2SideB = (sideC * Math.sin(Math.toRadians(t2AngleB))) / Math.sin(Math.toRadians(t2AngleC));
 
                 double robotYPosition = angleC;
-                double robotXPosition = sideB;
+                double robotXPosition = t2SideB;
                 double robotHeadingDegree = desiredTag.ftcPose.yaw;
 
 
-                double fieldX = aprilTagPose.getX() - robotYPosition;
-
-
-
-
+                //double fieldX = aprilTagPose.getX() - robotYPosition;
 
                 telemetry.addData("Robot Y",robotYPosition);
                 telemetry.addData("Robot X",robotXPosition);
@@ -128,19 +111,6 @@ public class TestApril extends LinearOpMode {
 
                 double tagPositionX = desiredTag.metadata.fieldPosition.get(0);
                 double tagPositionY = desiredTag.metadata.fieldPosition.get(1);
-
-                telemetry.addData("X", desiredTag.ftcPose.x);
-                telemetry.addData("Y", desiredTag.ftcPose.y);
-                telemetry.addData("Bearing", desiredTag.ftcPose.bearing);
-                telemetry.addData("Yaw", desiredTag.ftcPose.yaw);
-                telemetry.addData("Range",desiredTag.ftcPose.range);
-                testy = desiredTag.ftcPose.x - tagPositionY + 7; //add right number
-                testx = 63 - desiredTag.ftcPose.y; //add right number
-                testAngle = desiredTag.ftcPose.yaw; //may be a problem
-                if(testAngle < 0) {
-                    testAngle += 360;
-                }
-
 
            telemetry.update();
         }
